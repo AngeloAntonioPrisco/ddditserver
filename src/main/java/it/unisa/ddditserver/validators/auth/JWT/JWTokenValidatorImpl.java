9b9c014c30d0fb9342d20c70c7c256fff4932057
PasqualeSorrentino <119622217+PasqualeSorrentino@users.noreply.github.com>
2026-02-28 11:51:26 +0100

    Update JWTokenValidatorImpl.java, suggested by sonarqube.

--- src/main/java/it/unisa/ddditserver/validators/auth/JWT/JWTokenValidatorImpl.java
+++ src/main/java/it/unisa/ddditserver/validators/auth/JWT/JWTokenValidatorImpl.java
@@ -6,7 +6,6 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import it.unisa.ddditserver.db.cosmos.auth.CosmosAuthRepository;
 import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import javax.crypto.SecretKey;
