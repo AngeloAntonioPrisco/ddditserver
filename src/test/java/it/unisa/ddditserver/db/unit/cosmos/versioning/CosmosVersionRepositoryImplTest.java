d1adb621b9abf38b67119086dce6d2395c04d086
PasqualeSorrentino <119622217+PasqualeSorrentino@users.noreply.github.com>
2026-02-28 12:58:41 +0100

    Update CosmosVersionRepositoryImplTest.java, suggested by SonarQube.

--- src/test/java/it/unisa/ddditserver/db/unit/cosmos/versioning/CosmosVersionRepositoryImplTest.java
+++ src/test/java/it/unisa/ddditserver/db/unit/cosmos/versioning/CosmosVersionRepositoryImplTest.java
@@ -1,7 +1,5 @@
 package it.unisa.ddditserver.db.unit.cosmos.versioning;
 
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 // ATTENTION: at the moment due to time restrictions only tests for happy paths are available
 public class CosmosVersionRepositoryImplTest {
