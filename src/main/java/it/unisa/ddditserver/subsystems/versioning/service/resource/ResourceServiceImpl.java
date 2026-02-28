02ecf72aa8e3417c6666cba81592c0a430643878
PasqualeSorrentino <119622217+PasqualeSorrentino@users.noreply.github.com>
2026-02-28 11:45:31 +0100

    Update ResourceServiceImpl.java, suggested by sonarqube.

--- src/main/java/it/unisa/ddditserver/subsystems/versioning/service/resource/ResourceServiceImpl.java
+++ src/main/java/it/unisa/ddditserver/subsystems/versioning/service/resource/ResourceServiceImpl.java
@@ -19,7 +19,6 @@ import it.unisa.ddditserver.validators.versioning.repo.RepositoryValidationDTO;
 import it.unisa.ddditserver.validators.versioning.repo.RepositoryValidator;
 import it.unisa.ddditserver.validators.versioning.resource.ResourceValidationDTO;
 import it.unisa.ddditserver.validators.versioning.resource.ResourceValidator;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
