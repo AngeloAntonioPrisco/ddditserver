19318447b694b50733b6a98c930c22b35d7a7b25
PasqualeSorrentino <119622217+PasqualeSorrentino@users.noreply.github.com>
2026-02-28 11:46:03 +0100

    Update VersionControllerImpl.java, suggested by sonarqube.

--- src/main/java/it/unisa/ddditserver/subsystems/versioning/controller/version/VersionControllerImpl.java
+++ src/main/java/it/unisa/ddditserver/subsystems/versioning/controller/version/VersionControllerImpl.java
@@ -8,7 +8,6 @@ import it.unisa.ddditserver.subsystems.versioning.exceptions.resource.ResourceEx
 import it.unisa.ddditserver.subsystems.versioning.exceptions.version.*;
 import it.unisa.ddditserver.subsystems.versioning.service.version.VersionService;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
