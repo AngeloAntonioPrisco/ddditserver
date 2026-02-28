867017110e29adc2eef6414c8dcc9986b20c7219
PasqualeSorrentino <119622217+PasqualeSorrentino@users.noreply.github.com>
2026-02-28 11:51:11 +0100

    Update RepositoryControllerImpl.java, suggested by sonarqube.

--- src/main/java/it/unisa/ddditserver/subsystems/versioning/controller/repo/RepositoryControllerImpl.java
+++ src/main/java/it/unisa/ddditserver/subsystems/versioning/controller/repo/RepositoryControllerImpl.java
@@ -7,7 +7,6 @@ import it.unisa.ddditserver.subsystems.versioning.exceptions.repo.InvalidReposit
 import it.unisa.ddditserver.subsystems.versioning.exceptions.repo.RepositoryException;
 import it.unisa.ddditserver.subsystems.versioning.service.repo.RepositoryService;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
