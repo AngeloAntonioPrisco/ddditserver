14641ec444d61c445990814f26b5092403020aa3
Angelo Antonio Prisco <a.prisco50@studenti.unisa.it>
2026-02-28 11:51:14 +0100

    Removed unused import from AuthControllerImpl.java.

--- src/main/java/it/unisa/ddditserver/subsystems/auth/controller/AuthControllerImpl.java
+++ src/main/java/it/unisa/ddditserver/subsystems/auth/controller/AuthControllerImpl.java
@@ -4,7 +4,6 @@ import it.unisa.ddditserver.subsystems.auth.dto.UserDTO;
 import it.unisa.ddditserver.subsystems.auth.exceptions.*;
 import it.unisa.ddditserver.subsystems.auth.service.AuthService;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
